f5b925e217f30438b1067000cfd3b3d42e4bf650
yujhenchen <yujhen.chen93@gmail.com>
2021-12-10 09:44:24 +0800

    set global variables using pytest variables

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pytestBDD
 
 ## Run test
+
+### Via pytest
+
+pytest test_search_article.py -s
+
+### Via pytest BDD and generate BDD test report
--- tests/test_search_article.py
+++ tests/test_search_article.py
@@ -13,23 +13,6 @@ pytest.home = None
 
 @scenario("search_article.feature", "search keyword selenium")
 def test_search_article():
-    # try:
-    #     targetUrl = "https://blog.testproject.io/"
-    #     queryStr = "selenium"
-
-    #     driver = BrowserFactory.get_chrome()
-    #     driver.get(targetUrl)
-    #     driver.maximize_window()
-    #     home = HomePage(driver)
-    #     home = home.keyin_search(queryStr)
-    #     home = home.press_keyboard_enter()
-    #     home, searchResultsTitle = home.get_search_results_title(queryStr)
-    #     # verify query string in the search results title
-    #     assert (queryStr in searchResultsTitle)
-    # except Exception:
-    #     assert False
-    # finally:
-    #     driver.close()
     pass
 
 
