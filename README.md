5e67ec586cfe0102cc713eddb7774d1de553d378
yujhenchen <yujhen.chen93@gmail.com>
2021-12-10 13:49:44 +0800

    use pytest fixture to set before and after suite actions

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 pytest test_search_article.py -s
 
-### Via pytest BDD and generate BDD test report
+### Via pytest BDD and generate BDD test report (Not yet)
--- tests/test_search_article.py
+++ tests/test_search_article.py
@@ -3,14 +3,34 @@ from pytest_bdd import scenario, given, when, then
 from src.pages.home import HomePage
 from src.browsers.browserfactory import BrowserFactory
 
-# parameters
-pytest.targetUrl = "https://blog.testproject.io/"
-pytest.queryStr = "selenium"
-# variables
-pytest.home = None
+# # parameters
+# pytest.targetUrl = "https://blog.testproject.io/"
+# pytest.queryStr = "selenium"
+# # variables
+# pytest.home = None
 
 
+@pytest.fixture(scope="class")
+def set_global_variables():
+    # parameters
+    pytest.targetUrl = "https://blog.testproject.io/"
+    pytest.queryStr = "selenium"
+    # variables
+    pytest.home = None
+    pytest.driver = None
+    # print("before")  # debug use
+    yield
 
+
+@pytest.fixture(scope="class")
+def close_browsers():
+    yield
+    # print("after")  # debug use
+    if pytest.driver is not None:
+        pytest.driver.close()
+
+
+@pytest.mark.usefixtures("close_browsers", "set_global_variables")
 @scenario("search_article.feature", "search keyword selenium")
 def test_search_article():
     pass
@@ -18,10 +38,10 @@ def test_search_article():
 
 @given("navigate to the home page of the TestProject Test Automation Blog")
 def navigate_to_home():
-    driver = BrowserFactory.get_chrome()
-    driver.get(pytest.targetUrl)
-    driver.maximize_window()
-    pytest.home = HomePage(driver)
+    pytest.driver = BrowserFactory.get_chrome()
+    pytest.driver.get(pytest.targetUrl)
+    pytest.driver.maximize_window()
+    pytest.home = HomePage(pytest.driver)
 
 
 @when("I keyin the keyword in the keyword search field and click Enter on keyboard in the keyword search field")
@@ -32,6 +52,7 @@ def keyin_keyword():
 
 @then("search results title contain the keyword that I searched for")
 def contain_keyword():
-    pytest.home, searchResultsTitle = pytest.home.get_search_results_title(pytest.queryStr)
+    pytest.home, searchResultsTitle = pytest.home.get_search_results_title(
+        pytest.queryStr)
     # verify query string in the search results title
     assert (pytest.queryStr in searchResultsTitle)
