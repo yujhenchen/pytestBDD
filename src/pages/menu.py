05345f7130720fc3237aa9b0085676b6d82f42f7
yujhenchen <yujhen.chen93@gmail.com>
2021-12-10 17:45:50 +0800

    add menu page structure

---
+++ src/pages/menu.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from src.utils.waits import Waits
+
+
+class MenuPage(object):
+    def __init__(self):
+        super().__init__()
+
+        self.home = (By)
+        self.platform = (By)
+        self.docs = (By)
+        self.blog = (By)
+        self.forum = (By)
+        self.freeSignUp = (By)
+        self.login = (By)
+
+    def click_home(self):
+        return self
+
+    def click_platform(self):
+        return self
+
+    def click_docs(self):
+        return self
+
+    def click_blog(self):
+        return self
+
+    def click_forum(self):
+        return self
+
+    def click_freeSignUp(self):
+        return self
+
+    def click_login(self):
+        return self
